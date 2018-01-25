d762bf56d752066cdf64b19f42cda96020433b08
letheyue <sadameyy@hotmail.com>
2018-01-25 17:51:09 -0600

    Add view and field 1. add detail view 2. add imagefield

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
-from products.views import ProductListView, product_list_view
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 
 urlpatterns = [
 	url(r'^$', home_page),
@@ -29,9 +29,11 @@ urlpatterns = [
 	url(r'^contact/$', contact_page),
 	url(r'^login/$', login_page),
 	url(r'^register/$', register_page),
+    url(r'^products/$', ProductListView.as_view()),
+    url(r'^products-fbv/$', product_list_view),
+    url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
     url(r'^admin/', admin.site.urls),
-    url(r'^products/', ProductListView.as_view()),
-    url(r'^products-fbv/', product_list_view),
 ]
 
 if settings.DEBUG:
