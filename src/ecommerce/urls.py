cb963d77a679767ed669c6d2e755774791fd7958
letheyue <sadameyy@hotmail.com>
2018-01-28 00:29:42 -0600

    reverse url

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
 	url(r'^contact/$', contact_page),
 	url(r'^login/$', login_page),
 	url(r'^register/$', register_page),
-	url(r'^products/', include("products.urls")),
+	url(r'^product/', include("products.urls", namespace='products')),
     # url(r'^products/$', ProductListView.as_view()),
     # url(r'^products-fbv/$', product_list_view),
     # # url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
