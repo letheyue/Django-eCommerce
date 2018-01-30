14a58b420d33b333dc2c4c78c342b2c74a3ffc55
letheyue <sadameyy@hotmail.com>
2018-01-29 23:47:54 -0600

    add search form

--- src/search/views.py
+++ src/search/views.py
@@ -5,15 +5,21 @@ from products.models import Product
 # Create your views here.
 class SearchProductView(ListView):
 
-	template_name = "products/list.html"
+	template_name = "search/view.html"
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+		query = self.request.GET.get('q')
+		context['query'] = query
+		# SearchQuery.objects.create(query=query)
+		return context
 
 	def get_queryset(self, *args, **kwargs):
 		request = self.request
 		method_dict = request.GET
 		query = method_dict.get('q', None) # method_dict['q']
-		print(query)
 		if query is not None:
-			return Product.objects.filter(title__icontains='hat')
+			return Product.objects.filter(title__icontains=query)
 		return Product.objects.featured() 
 		# __icontains = field contains this __iexact = fields is exactly this
 		# url parameter ?q=hat
