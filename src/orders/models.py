aaace9f3215a10f0d9731cd15ddc2b68cc330202
letheyue <sadameyy@hotmail.com>
2018-03-03 00:55:31 -0600

    checkout view

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.signals import pre_save, post_save
+from decimal import Decimal
 
 from carts.models import Cart
 from ecommerce.utils import unique_order_id_generator 
@@ -29,7 +30,7 @@ class Order(models.Model):
     def update_total(self):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
-        new_total = cart_total + shipping_total
+        new_total = Decimal(cart_total) + Decimal(shipping_total)
         self.total = new_total
         self.save()
         return new_total
