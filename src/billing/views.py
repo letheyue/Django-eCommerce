43a65d6b6a62382b874dbc83164621efae170ff8
letheyue <sadameyy@hotmail.com>
2018-03-10 16:05:08 -0600

    edit payment method form

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+
+from django.utils.http import is_safe_url
 
 import stripe
 stripe.api_key = "sk_test_Vsa3Q1q8k6YRX2J600IWWzjl"
@@ -8,6 +11,18 @@ STRIPE_PUB_KEY = 'pk_test_zrQeiyEJDcWZZUouU084QErp'
 
 
 def payment_method_view(request):
-    if request.method == "POST":
+    #next_url = 
+    next_url = None
+    next_ = request.GET.get('next')
+    if is_safe_url(next_, request.get_host()):
+        next_url = next_
+    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY, "next_url": next_url})
+
+
+
+
+def payment_method_createview(request):
+    if request.method == "POST" and request.is_ajax():
         print(request.POST)
-    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY})
+        return JsonResponse({"message": "Success! Your card was added."})
+    return HttpResponse("error", status_code=401)
